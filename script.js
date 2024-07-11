00dccdf8cf0020e844c503a749d8492d4afdb2cc
Thomas-Tubinas <143620957+Thomas-Tubinas@users.noreply.github.com>
2024-07-11 10:57:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,21 +2,21 @@
 <html>
 <body>
 
+    <!-- WELCOME -->
+    <div class="heading">Hello!</div>
+    <hr>
+    <p>
+    My name is Thomas Tubinas. I am an student at Arcadia University with fueled by an 
+    unwavering enthusiasm for the field of cybersecurity, actively contributing to the IT 
+    sector while also making waves as a valuable member of the school's swim team.
+    </p>
+
 <h2>What Can JavaScript Do?</h2>
 
 <p id="demo">JavaScript can change HTML content.</p>
-
-    <!-- WELCOME -->
-    <div class="heading">Welcome to my site!</div>
-    <hr>
-    <p>
-      This is my personal website where you can find info about myself, project showcases, and documentation I have
-      gathered over the years.
-    </p>
-
-<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Projects</button>
-<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello World!"'>Certifications</button>
-<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello World!"'>In Progress</button>
+<button type="button1" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Projects</button>
+<button type="button2" onclick='document.getElementById("demo").innerHTML = "Hello World!"'>Certifications</button>
+<button type="button3" onclick='document.getElementById("demo").innerHTML = "Mad World!"'>In Progress</button>
 
 </body>
 </html>
